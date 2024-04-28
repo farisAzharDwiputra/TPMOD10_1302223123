95b873089ff9c71843ae309cd3cf0121a0038c3e
unknown <farisazhar.0241@gmail.com>
2024-04-29 00:48:59 +0700

    revisi commit API

--- tpmodul10_1302223123/Controllers/ControllerMahasiswa.cs
+++ tpmodul10_1302223123/Controllers/ControllerMahasiswa.cs
@@ -9,8 +9,8 @@ namespace tpmodul10_1302223123.Controllers
     {
         private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
         {
-            new Mahasiswa ("Farah Amalia Putri", "1302223137" ),
             new Mahasiswa ("Faris Azhar Dwiputra", "1302223123" ),
+            new Mahasiswa ("Farah Amalia Putri", "1302223137" ),
             new Mahasiswa ("Wildan Syukri Ni'am", "1302220005"),
             new Mahasiswa ("Muhammad Bintang Al-Fath", "1302223099"),
             new Mahasiswa ("Arga Adolf Lumunon", "1302223038"),
